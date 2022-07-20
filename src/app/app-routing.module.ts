f5e862fdef371f7b77449165c40e588d3dde8111
DESKTOP-KO67VDE\Constanza <constanza.luduena@col.vueling.com>
2022-07-20 09:41:46 +0200

    Bombon component closes #15

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BellotaComponent } from './components/bellota/bellota.component';
+import { BombonComponent } from './components/bombon/bombon.component';
 import { BurbujaComponent } from './components/burbuja/burbuja.component';
 import { HomeComponent } from './components/home/home.component';
 
@@ -8,9 +10,9 @@ const routes: Routes = [
   { path: "", component: HomeComponent},
   { path: "home", component: HomeComponent},
   { path: "burbuja", component: BurbujaComponent, pathMatch:"full"},
-  //{ path: "bombon", component: BombonComponent, pathMatch:"full"},
-  //{ path: "bellota", component: BellotaComponent, pathMatch:"full"},
-  { path: "**", component: HomeComponent}
+  { path: "bombon", component: BombonComponent, pathMatch:"full"},
+  { path: "bellota", component: BellotaComponent, pathMatch:"full"},
+  { path: "**", component: HomeComponent} //AGREGAR ErrorComponent
 ];
 
 @NgModule({
